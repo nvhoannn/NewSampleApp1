dfe43b3248935eb0716a52105cf971b861743641
Hoan <tryingforlif3@gmail.com>
2022-10-25 20:31:25 +0700

    finish advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },
       format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
     before_save :downcase_email
   
@@ -17,8 +18,27 @@ class User < ApplicationRecord
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
       end
-    end
   
+      def new_token
+        SecureRandom.urlsafe_base64
+      end
+    end
+
+    def remember
+      self.remember_token = User.new_token
+      update(remember_digest: User.digest(remember_token))
+    end
+    
+    # Forgets a user.
+    def forget
+      update(remember_digest: nil)
+    end
+
+    # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
     private
     def downcase_email
       email.downcase!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_141506) do
+ActiveRecord::Schema.define(version: 2022_10_25_122430) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_10_12_141506) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
