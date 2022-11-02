45d0cf62d18fead1671715da9fe1fd74f840fcb3
Hoan <tryingforlif3@gmail.com>
2022-11-02 20:25:50 +0700

    finish_reset_password

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
 
