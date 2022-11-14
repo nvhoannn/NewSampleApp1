108dba76822c516ca016a0ef48f29fa229b4a9e4
Hoan <tryingforlif3@gmail.com>
2022-11-14 20:44:53 +0700

    micropost

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,3 +29,11 @@ document.addEventListener("turbolinks:load", () => {
         return new Popover(popoverTriggerEl)
     })
 })
+
+$(document).on("change", "#micropost_image", function () {
+    var size_in_megabytes = this.files[0].size / 1024 / 1024;
+  
+    if (size_in_megabytes > 5) {
+      alert("Maximum file size is 0.1MB. Please choose a smaller file.");
+    }
+  });
